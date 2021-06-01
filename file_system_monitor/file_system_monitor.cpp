a50c89b75ec833ef626ba4084a13ca7773fa735b
Mordechai Yaakobi <myaakobi@gmail.com>
2021-06-01 09:46:50 +0300

    Fixing close handle when termination

--- file_system_monitor/file_system_monitor.cpp
+++ file_system_monitor/file_system_monitor.cpp
@@ -29,37 +29,42 @@ BOOL WINAPI consoleHandler(DWORD signal) {
 int main(int argc, char** argv)
 {
     notifyHandle = ::CreateEvent(NULL, TRUE, FALSE, NULL); 
-    if (INVALID_HANDLE_VALUE == notifyHandle) {
-        std::cout << "\nError creating stop event, aborting ... " << std::endl; 
-        return 1; 
-    }
-    if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
-        std::cout << "\nERROR: Could not set control handler" << std::endl; 
-        return 2;
-    }
-    std::unique_ptr<FileMonitorImpWin> pFileMonitor(new FileMonitorImpWin());
-    //Assuming input is valid directory names - otherwise there is need to add validation code 
-    for (int idx = 1; idx < argc; ++idx) {
-        String DirectoryName(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(argv[idx]));
-        listenerIdVector.push_back(pFileMonitor->addFileMonitorListener(DirectoryName));
+    
+        if (INVALID_HANDLE_VALUE == notifyHandle) {
+            std::cout << "\nError creating stop event, aborting ... " << std::endl;
+            return 1;
+        }
+        if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
+            std::cout << "\nERROR: Could not set control handler" << std::endl;
+            return 2;
+        }
+        std::unique_ptr<FileMonitorImpWin> pFileMonitor(new FileMonitorImpWin());
+        //Assuming input is valid directory names - otherwise there is need to add validation code 
+        for (int idx = 1; idx < argc; ++idx) {
+            String DirectoryName(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(argv[idx]));
+            listenerIdVector.push_back(pFileMonitor->addFileMonitorListener(DirectoryName));
 
-    }
-    std::cout << "\n Press Ctrl + C to stop monitoring" << std::endl; 
-    DWORD dwWaitResult = ::WaitForSingleObject(notifyHandle, INFINITE); 
-    switch (dwWaitResult) {
+        }
+        std::cout << "\n Press Ctrl + C to stop monitoring" << std::endl;
+        DWORD dwWaitResult = ::WaitForSingleObject(notifyHandle, INFINITE);
+        switch (dwWaitResult) {
         case WAIT_OBJECT_0:
             std::cout << "Stop monitoring cleanup ..." << std::endl;
             std::for_each(listenerIdVector.begin(), listenerIdVector.end(),
                 [&](ListenerID listenerid) {
                     pFileMonitor->removeListener(listenerid);
                 });
+            ::CloseHandle(notifyHandle);
             break;
-        default: 
+        default:
             std::cout << " Error waiting for stop notify " << ::GetLastError() << std::endl;
-            std::cout << "Aborting ... " << std::endl; 
-            abort(); 
+            std::cout << "Aborting ... " << std::endl;
+            ::CloseHandle(notifyHandle);
+            abort();
 
-    } 
+        }
+
+   ::CloseHandle(notifyHandle);
    
     
    
